d418e290d89f436d0c7baef1b7414d6a3515e98f
degraevedoran <doran.degraeve@student.howest.be>
2021-02-15 15:04:04 +0100

    video + nav

--- frontend-bloom/src/App.js
+++ frontend-bloom/src/App.js
@@ -2,7 +2,36 @@ import React, {useState, useEffect} from 'react';
 import './styles/style.scss';
 import Navigatie__item from './components/Navigatie.js';
 import Navigatie__logo from './components/NavigatieLogo.js';
+import Video from './components/Video.js';
 
+
+const mockMovie = {
+  "id": 1,
+  "published_at": "2021-02-13T19:55:19.003Z",
+  "created_at": "2021-02-13T19:55:07.858Z",
+  "updated_at": "2021-02-13T19:55:19.036Z",
+  "Video": [
+      {
+          "id": 2,
+          "name": "Intro MGM Lion.mp4",
+          "alternativeText": "",
+          "caption": "",
+          "width": null,
+          "height": null,
+          "formats": null,
+          "hash": "Intro_MGM_Lion_695e445ecd",
+          "ext": ".mp4",
+          "mime": "video/mp4",
+          "size": 504.52,
+          "url": "/uploads/Intro_MGM_Lion_695e445ecd.mp4",
+          "previewUrl": null,
+          "provider": "local",
+          "provider_metadata": null,
+          "created_at": "2021-02-13T19:54:51.428Z",
+          "updated_at": "2021-02-13T19:54:51.445Z"
+      }
+  ]
+}
 
 function App() {
 
@@ -18,6 +47,18 @@ function App() {
     getNavigaties()
   }, [])
 
+  const [videos, setVideos] = useState([])
+
+  useEffect(() => {
+    const getVideos = async () => {
+      const response = await fetch('http://localhost:1337/video-urls')
+      const data = await response.json()
+      setVideos(data)
+    }
+
+    getVideos()
+  }, [])
+
 
   return (
     <div className="App">
@@ -25,15 +66,26 @@ function App() {
       <div className="Navigatie">
         
         <Navigatie__logo />
-      <div className="Navigatie__items">
-        {navigaties.map(navigatie => (
-            <Navigatie__item 
-              navigatie={navigatie.NavigatieLink}
-            />
-          ))}
-      </div>
+
+        <div className="Navigatie__items">
+          {navigaties.map(navigatie => (
+              <Navigatie__item 
+                navigatie={navigatie.NavigatieLink}
+              />
+            ))}
+        </div>
         
       </div>
+      <div className="Video__container">
+        {videos.map(video => (
+          <Video 
+            url={video.url}
+            
+          />
+        ))}    
+          
+      </div>
+     
 
     </div>
   );
--- frontend-bloom/src/components/Video.js
+++ frontend-bloom/src/components/Video.js
@@ -1,41 +1,24 @@
 import React from 'react'
+import ReactPlayer from 'react-player'
 
 
-export default () => {
 
-    
-    const movie = {
-        "id": 1,
-        "published_at": "2021-02-13T19:55:19.003Z",
-        "created_at": "2021-02-13T19:55:07.858Z",
-        "updated_at": "2021-02-13T19:55:19.036Z",
-        "Video": [
-            {
-                "id": 2,
-                "name": "Intro MGM Lion.mp4",
-                "alternativeText": "",
-                "caption": "",
-                "width": null,
-                "height": null,
-                "formats": null,
-                "hash": "Intro_MGM_Lion_695e445ecd",
-                "ext": ".mp4",
-                "mime": "video/mp4",
-                "size": 504.52,
-                "url": "/uploads/Intro_MGM_Lion_695e445ecd.mp4",
-                "previewUrl": null,
-                "provider": "local",
-                "provider_metadata": null,
-                "created_at": "2021-02-13T19:54:51.428Z",
-                "updated_at": "2021-02-13T19:54:51.445Z"
-            }
-        ]
-    }
 
+
+export default ({url}) => {
 
     return (
         <div className="Video">
-             
+          <div className="player-wrapper">
+            <ReactPlayer
+              className="react-player" 
+              url={url}
+              controls = "true"
+              width='1280px'
+              height='720px'
+            />
+          </div>
+          
         </div>  
     )
 }
