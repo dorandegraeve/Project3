b7490dbb43b557515b7c4267f33fc7d245465930
degraevedoran <doran.degraeve@student.howest.be>
2021-02-16 11:47:23 +0100

    architecture

--- frontend-bloom/src/App.js
+++ frontend-bloom/src/App.js
@@ -1,86 +1,55 @@
-import React, {useState, useEffect} from 'react';
-import './styles/style.scss';
+import React,{useState, useEffect} from 'react';
+import {BrowserRouter, Switch, Route} from 'react-router-dom'
+
 import Navigatie__item from './components/Navigatie.js';
 import Navigatie__logo from './components/NavigatieLogo.js';
-import Video from './components/Video.js';
-import Card from './components/Card';
 
+
+import Home from './pages/Home'
+import Product from './pages/Product'
+
+import './styles/style.scss';
 
 function App() {
 
   const [navigaties, setNavigaties] = useState([])
 
-  useEffect(() => {
-    const getNavigaties = async () => {
-      const response = await fetch('http://localhost:1337/navigaties')
-      const data = await response.json()
-      setNavigaties(data)
-    }
+    useEffect(() => {
+        const getNavigaties = async () => {
+        const response = await fetch('http://localhost:1337/navigaties')
+        const data = await response.json()
+        setNavigaties(data)
+        }
 
-    getNavigaties()
-  }, [])
-
-  const [videos, setVideos] = useState([])
-
-  useEffect(() => {
-    const getVideos = async () => {
-      const response = await fetch('http://localhost:1337/video-urls')
-      const data = await response.json()
-      setVideos(data)
-    }
-
-    getVideos()
-  }, [])
-
-  const [cards, setCards] = useState([])
-
-  useEffect(() => {
-    const getCards = async () => {
-      const response = await fetch('http://localhost:1337/cards')
-      const data = await response.json()
-      setCards(data)
-    }
-    
-    getCards()
-  }, [])
+        getNavigaties()
+    }, [])
 
 
   return (
     <div className="App">
 
-      <div className="Navigatie">
-        
-        <Navigatie__logo />
+      
+      <BrowserRouter>
+        <div className="Navigatie">
+          
+          <Navigatie__logo />
 
-        <div className="Navigatie__items">
+          <div className="Navigatie__items">
           {navigaties.map(navigatie => (
               <Navigatie__item 
-                navigatie={navigatie.NavigatieLink}
+                  navigatie={navigatie.NavigatieLink}
               />
-            ))}
+              ))}
+          </div>
         </div>
-        
-      </div>
-      <div className="Video__container">
-        {videos.map(video => (
-          <Video 
-            url={video.url}
-            
-          />
-        ))}    
-          
-      </div>
-      <h1 className="titel">Onze visie</h1>
-     <div className="Kaartjes">
-      {cards.map(card => (
-        <Card 
-          Tekstje ={card.Tekstje}
-          Beschrijving={card.Beschrijving}
-          url={card.Image[0] && card.Image[0].url}
-        />
-      ))}
-    </div>
-     
+
+
+        <Switch>
+          <Route path="/home" exact component={Home} />
+          <Route path="/product" exact component={Product} />
+        </Switch>
+      </BrowserRouter>
+      
 
     </div>
   );
