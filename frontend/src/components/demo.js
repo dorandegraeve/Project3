acc199f2314cd1c45a1a23c243a0b3cbf58eb1d9
TiboVandenabeele <43953582+TiboVandenabeele@users.noreply.github.com>
2021-03-03 14:08:07 +0100

    mailchimp demo

---
+++ frontend/src/components/demo.js
@@ -0,0 +1,65 @@
+import React from 'react'
+import addToMailchimp from 'gatsby-plugin-mailchimp'
+
+export default class IndexPage extends React.Component {
+    state = {
+        name: null,
+        email: null,
+    }
+
+    _handleChange = e => {
+        console.log({
+            [`${e.target.name}`]: e.target.value,
+        })
+        this.setState({
+            [`${e.target.name}`]: e.target.value,
+        })
+    }
+
+    _handleSubmit = e => {
+        e.preventDefault()
+
+        console.log('submit', this.state)
+
+        addToMailchimp(this.state.email, this.state, 'https://howest.us20.list-manage.com/subscribe/post?u=f080a4f54ee3661e4a8efddae&amp;id=9d882d82ec')
+            .then(({ msg, result }) => {
+                console.log('msg', `${result}: ${msg}`)
+
+                if (result !== 'success') {
+                    throw msg
+                }
+                alert(msg)
+            })
+            .catch(err => {
+                console.log('err', err)
+                alert(err)
+            })
+    }
+
+    render() {
+        return (
+            <div className="demo__volledig">
+
+                <div>
+                    <form className="" onSubmit={this._handleSubmit}>
+                        <input
+                            className="demo__field"
+                            type="email"
+                            onChange={this._handleChange}
+                            placeholder="Jouw e mailadres"
+                            name="email"
+                            style={{
+                                paddingLeft: "3.2rem",
+                                fontSize: "2rem",
+                                color: "#98A3BD" ,
+                            }}
+                        />
+                        <br />
+                        <br />
+                        <input className ="button button--demo" type="submit" value="Verstuur code"/>
+                    </form>
+                </div>
+            </div>
+        )
+    }
+}
